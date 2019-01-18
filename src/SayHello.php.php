c6a69192eb93117d53149254e9d874612c913022
zhengtaoyu <717828758@qq.com>
2019-01-18 13:54:15 +0800

    1

---
+++ src/SayHello.php.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* this is a test for composer package.
+* Date: 2017/11/3
+* Time: 16:51
+*/
+namespace Hello;
+class SayHello {
+    public static function world () {
+        return 'Hello World!';
+    }
+}
